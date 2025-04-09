ba5ee95d67d9f19f2734972c5777c324b6bc8aa0
o7digital <101292703+o7digital@users.noreply.github.com>
2025-04-09 15:02:11 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
-
-
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'http://lcqc.vercel.app'
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+output: 'statit',
 });
