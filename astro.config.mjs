eee22eeb9dfbc6c0622c31ffc5dedfdfc1c67e3a
o7digital <101292703+o7digital@users.noreply.github.com>
2025-04-13 19:43:48 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind'; // Integrador oficial de Tailwind
-import { tinaPlugin } from "tinacms";
 
 export default defineConfig({
   site: 'https://lcqc.vercel.app',  // ¡Ojo con la coma aquí!
