8fd1b66d5d19d73d6b944b17477cf623d267f489
o7digital <101292703+o7digital@users.noreply.github.com>
2025-04-16 14:25:03 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import { tinaPlugin } from 'tinacms'; // 👈 AGREGA ESTA LÍNEA
+// import { tinaPlugin } from 'tinacms'; // 👈 ESTA LÍNEA ESTÁ COMENTADA
 
 export default defineConfig({
   site: 'https://lcqc.vercel.app',
   integrations: [
     tailwind(),
-    tinaPlugin({
-      clientId: '47bc5fe7-6887-4669-a245-f6c384126521',
-      branch: 'main',
-    }),
+    // tinaPlugin({  // ESTA LÍNEA ESTÁ COMENTADA
+    //   clientId: '47bc5fe7-6887-4669-a245-f6c384126521',
+    //   branch: 'main',
+    // }),
   ],
   output: 'static',
 });
